e2dfc76a38b65e2dd0f3c4acf36c09502c031ead
studentcompsci <lgaselle@xula.edu>
2018-01-31 10:51:49 -0600

    set

--- src/edu/xula/klewis/Main.java
+++ src/edu/xula/klewis/Main.java
@@ -1,19 +1,44 @@
 package edu.xula.klewis;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\n Hello World!");
+        System.out.println("\n Hello World!\n");
 
+        final int MAX_VALUE= 10;
 
-        ArrayList<Integer> list = randomNums(10, 20);
+        ArrayList<Integer> list = randomNums(10, MAX_VALUE);
         System.out.println("Random list "+ list);
+
+        //what are unique values are in a list
+        Set<Integer>values=uniqueValues(list);
+        System.out.println("unique values are "+ values);
+
+        ArrayList<Integer>missingElements=whosMissing(list,MAX_VALUE);
     }
 
+    private static ArrayList<Integer> whosMissing(ArrayList<Integer> list,int max) {
+        ArrayList<Integer>missingNums=new ArrayList<>();
+        for (int i = 0; i <list.size() ; i++) {
 
+
+        }
+    }
+
+    /**
+ * which elements are missing?
+   *
+    */
+
+    public static Set<Integer> uniqueValues(ArrayList<Integer>list){
+        Set<Integer>storeUnique=new HashSet<>();
+        for (int i = 0; i <list.size() ; i++) {
+            storeUnique.add(list.get(i))
+        }
+        return storeUnique;
+    }
 
     private static ArrayList<Integer> randomNums(int num, int bound) {
         ArrayList<Integer> numbers = new ArrayList<>();
